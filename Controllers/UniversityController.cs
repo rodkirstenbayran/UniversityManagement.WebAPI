939b1978618cbf11693517e74a589a2b4c203d2c
rodkirstenbayran <162873188+rodkirstenbayran@users.noreply.github.com>
2024-10-17 14:34:22 +0800

    Add files via upload

---
+++ Controllers/UniversityController.cs
@@ -0,0 +1,103 @@
+﻿using UniversityHub.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace UniversityHub.Controllers
+{
+    public class UniversityController : Controller
+    {
+        // GET: UniversityController
+        public async Task<ActionResult> Index()
+        {
+            string apiUrl = "https://localhost:7070/api/university";
+
+            List<University> university = new List<University>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                var result = await response.Content.ReadAsStringAsync();
+                university = JsonConvert.DeserializeObject<List<University>>(result);
+            }
+
+            return View(university);
+        }
+
+        // GET: UniversityController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UniversityController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UniversityController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(University university)
+        {
+            string apiUrl = "https://localhost:7070/api/university";
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(university), Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(university);
+        }
+
+        // GET: UniversityController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UniversityController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UniversityController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UniversityController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/University.cs
@@ -0,0 +1,10 @@
+﻿namespace UniversityHub.Models
+{
+    public class University
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+        public string location { get; set; }
+        public string courses { get; set; }
+    }
+}
